36eac00616dc2c25798a36ea38156fc5e5195382
Urja4157 <urjabajj2013@gmail.com>
2024-06-01 22:40:46 +0545

    Add project files.

---
+++ ASPCoreWebAPICRUD/Models/MyNewDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPICRUD.Models
+{
+    public class MyNewDbContext : DbContext
+    {
+        public MyNewDbContext(DbContextOptions<MyNewDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        
+    }
+    
+}
---
+++ ASPCoreWebAPICRUD/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASPCoreWebAPICRUD.Models
+{
+    public partial class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string StudentName { get; set; }
+
+        [MaxLength(50)]
+        public string StudentGender { get; set; }
+
+        public int Age { get; set; }
+
+        public int Standard { get; set; }
+
+        [MaxLength(255)]
+        public string FatherName { get; set; }
+    }
+}
